44a9e32918ac76b0699e5624f978927edd989bd1
ethereal element <vitalik.safonov@list.ru>
2023-04-23 20:59:01 +0300

    install: scss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
+import React from 'react';
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
-
-export default App;
+export const App = () => {
+  return <div>Петух
+  </div>;
+};
