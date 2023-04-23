3a62592ead8983a9cd1b9448c685d5b9ce015d56
ethereal element <vitalik.safonov@list.ru>
2023-04-23 22:19:26 +0300

    create: header layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import React from 'react';
+import "./App.scss"
 
 export const App = () => {
-  return <div>Петух
-  </div>;
+  return <>
+    <header className="header" >
+      <div className="header__content">
+        <a href="#">
+        <img src="/images/logo.png" alt="logo" width={40} height={40}/>
+        </a>
+        <div className="header__content-wrap">
+        <h3 className="header__title">REACT SNEAKERS</h3>
+        <p className="logo__sub">
+        Магазин лучших кроссовок
+        </p>
+        </div>
+      </div>
+
+        <div className="header__login">
+          <div className="header__login-box">
+            <div className="header__loghin-inner">
+            <img src="./images/shop-icon.svg" alt="shop" />
+            <span className="header__login-spn">1205 руб.</span>
+            </div>
+            <img src="./images/favorites-icon.svg" alt="" />
+          <img src="./images/login-icon.svg" alt="" />
+          </div>
+        </div>
+    </header>
+  </>;
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import {App} from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import { App } from "./App";
+import "./helpers/reset.scss";
+import "macro-css"
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="wrapper">
+      <div className="container">
+        <App />
+      </div>
+    </div>
   </React.StrictMode>
 );
-
