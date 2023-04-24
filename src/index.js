4c96a3319e9eededb586dc3068e496e1c4129fa7
Сафонов Виталий Геннадьевич <safonovvg@mirhold.local>
2023-04-24 11:33:26 +0300

    fix: add margin top header

--- src/index.js
+++ src/index.js
@@ -9,10 +9,6 @@ import "macro-css"
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div className="wrapper">
-      <div className="container">
         <App />
-      </div>
-    </div>
   </React.StrictMode>
 );
