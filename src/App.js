5aca487ac21272097eff3ab8a0ca0dd384db760a
Сафонов Виталий Геннадьевич <safonovvg@mirhold.local>
2023-04-25 13:56:39 +0300

    create: card item+hover

--- src/App.js
+++ src/App.js
@@ -34,7 +34,34 @@ export const App = () => {
             <section className="products">
               <h1 className="products__title">Все кроссовки</h1>
             </section>
-            
+
+            <ul className="card-list">
+              <li className="card-list__item">
+                <img
+                  src="/images/card-image.jpg"
+                  alt=""
+                  className="card-list__img"
+                  width="133px"
+                />
+                <div className="card-list__inner">
+                <p className="card-list__title">
+                  Мужские Кроссовки
+                </p>
+                <p className="card-list__subtitle">
+                Nike Blazer Mid Suede
+                </p>
+                </div>
+                <div className="card-wrap">
+                  <div className="card-inner">
+                    <span className="card__list-sub">Цена:</span>
+                    <p className="card__list-price">12 999 руб.</p>
+                  </div>
+                  <div className="card-button__wrap">
+                    <img src="/images/add-item.svg" alt="" />
+                  </div>
+                </div>
+              </li>
+            </ul>
           </div>
         </main>
       </div>
