dbd64c7e7e78b82594573c08260b78092c9e1776
ethereal element <vitalik.safonov@list.ru>
2023-04-28 20:02:50 +0300

    fix: drawer on module.scss

--- src/components/drawer/Drawer.jsx
+++ src/components/drawer/Drawer.jsx
@@ -1,75 +1,54 @@
 import React from "react";
-import "./Drawer.scss";
+import styles from "./drawer.module.scss";
 
 function Drawer() {
   return (
     <>
-      <div style={{ display: "none" }} className="overlay">
-        <div className="drawer">
+      <div style={{ display: "flex" }} className={styles.Overlay}>
+        <div className={styles.Drawer}>
           <div className="drawer__inner">
-            <h2 className="drawer__title">
+            <h2 className={styles.DrawerTitle}>
               Корзина
               <img src="images/btn-close.svg" className="close-btn" alt="" />
             </h2>
 
             <ul className="cart">
-              <li className="cart__item">
+              <li className={styles.CartItem}>
                 <img
                   width="70px"
                   height="70px"
                   src="images/shop-icon-2.jpg"
                   alt="shop"
                 />
-                <div className="cart__item-box">
-                  <p className="cart__item-title">
+                <div className={styles.CartItemBox}>
+                  <p className={styles.CartItemTitle}>
                     Мужские Кроссовки Nike Air Max 270
                   </p>
                   <b className="cart__item-sub">12 999 руб.</b>
                 </div>
-                <button className="delete-button">
+                <button className={styles.DeleteButton}>
                   <img
                     src="images/btn-close.svg"
-                    className="close-btn"
+                    className={styles.CloseBtn}
                     alt=""
-                  />
-                </button>
-              </li>
-              <li className="cart__item">
-                <img
-                  width="70px"
-                  height="70px"
-                  src="images/shop-icon-3.jpg"
-                  alt="shop"
-                />
-                <div className="cart__item-box">
-                  <p className="cart__item-title">
-                    Мужские Кроссовки Nike Air Max 270
-                  </p>
-                  <b className="cart__item-sub">8 499 руб.</b>
-                </div>
-                <button className="delete-button">
-                  <img
-                    src="images/btn-close.svg"
-                    alt=""
-                    className="close-btn"
                   />
                 </button>
               </li>
             </ul>
           </div>
 
-          <div className="total">
-            <div className="total-1">
+          <div className={styles.Total}>
+            <div className={styles.TotalFirst}>
               <p className="total__title">Итого: </p>
               <div className="border"></div>
               <p className="total__price">21 498 руб.</p>
             </div>
-            <div className="total-2">
-              <p className="total__title">Налог 5%:</p>
-              <div className="border"></div>
-              <p className="total__price">1074 руб.</p>
+            <div className={styles.TotalSecond}>
+              <p className={styles.TotalTitle }>Налог 5%:</p>
+              <div className={styles.Border}></div>
+              <p className={styles.TotalPrice}>1074 руб.</p>
             </div>
-            <button className="button__buy">
+            <button className={styles.ButtonBuy}>
               Оформить заказ
               <img src="/images/arrow-left.svg" alt="arrow-icon" />
             </button>
