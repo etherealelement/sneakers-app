a45841d1e89560e7b798d86e80df72ff3be55267
Сафонов Виталий Геннадьевич <safonovvg@mirhold.local>
2023-04-29 16:50:50 +0300

    create: sneakers json database

--- src/App.js
+++ src/App.js
@@ -7,33 +7,89 @@ import Drawer from "./components/drawer/Drawer";
 
 const Product = [
   {
-    type: "Мужские кроссовки",
-    name: "Nike Blazer Mid Suede",
-    price: 12344,
-    id: 1,
-    image: "/images/card-image.jpg"
+    "type": "Мужские кроссовки",
+    "name": "Nike Blazer Mid Suede",
+    "price": 12344,
+    "id": 1,
+    "image": "/sneakers-item/1.jpg"
   },
   {
-    type: "Мужские кроссовки",
-    name: "Nike Air Max 270",
-    price: 15000,
-    id: 2,
-    image: "/images/shop-icon-2.jpg"
+    "type": "Мужские Кроссовки",
+    "name": "Nike Air Max 270",
+    "price": 12999,
+    "id": 2,
+    "image": "/sneakers-item/2.jpg"
   },
   {
-    type: "Мужские кроссовки",
-    name: "Nike Blazer Mid Suede",
-    price: 6999,
-    id: 3,
-    image: "/images/shop-icon-3.jpg"
+    "type": "Мужские Кроссовки",
+    "name": "Nike Blazer Mid Suede",
+    "price": 8499,
+    "id": 3,
+    "image": "/sneakers-item/3.jpg"
   },
   {
-    type: "Женские кроссовки",
-    name: "Under Armour Curry 8",
-    price: 12344,
-    id: 4,
-    image: "/images/shop-icon-4.jpg"
+    "type": "Мужские Кроссовки",
+    "name": "Puma X Aka Boku Future",
+    "price": 8999,
+    "id": 4,
+    "image": "/sneakers-item/4.jpg"
   },
+  {
+    "type": "Мужские Кроссовки",
+    "name": "Under Armour Curry 8",
+    "price": 15199,
+    "id": 5,
+    "image": "/sneakers-item/5.jpg"
+  },
+  {
+    "type": "Мужские Кроссовки",
+    "name": "Nike Kyrie 7",
+    "price": 11299,
+    "id": 6,
+    "image": "/sneakers-item/6.jpg"
+  },
+  {
+    "type": "Мужские Кроссовки",
+    "name": "Jordan Air Jordan 11",
+    "price": 10799,
+    "id": 7,
+    "image": "/sneakers-item/7.jpg"
+  },
+  {
+    "type": "Мужские Кроссовки",
+    "name": "Nike LeBron XVIII",
+    "price": 16499,
+    "id": 8,
+    "image": "/sneakers-item/8.jpg"
+  },
+  {
+    "type": "Мужские Кроссовки",
+    "name": "Nike Lebron XVIII Low",
+    "price": 13999,
+    "id": 9,
+    "image": "/sneakers-item/9.jpg"
+  },
+  {
+    "type": "Мужские Кроссовки",
+    "name": "Nike Blazer Mid Suede",
+    "price": 8499,
+    "id": 10,
+    "image": "/sneakers-item/10.jpg"
+  },
+  {
+    "type": "Мужские Кроссовки",
+    "name": "Aka Boku Future Rider",
+    "price": 8999,
+    "id": 11,
+    "image": "/sneakers-item/11.jpg"
+  },
+  {
+    "type": "Мужские Кроссовки",
+    "name": "Nike Kyrie Flytrap IV",
+    "price": 11299,
+    "id": 12,
+    "image": "/sneakers-item/12.jpg"
+  }
 ];
 
 
